2e5351da665357999ffd53cab1b96d3aed1432d2
morayl <sea.gang.moray@gmail.com>
2016-10-22 15:52:20 +0900

    [modify] コメント修正・メソッドの並び順修正 changeLogTag→setLogTagに修正※
    ※setメソッドの方が一般的で探しやすいため

--- footprint/src/main/java/com/morayl/footprint/Footprint.java
+++ footprint/src/main/java/com/morayl/footprint/Footprint.java
@@ -20,21 +20,211 @@ import java.lang.reflect.Field;
  * それを書いた場所のクラス名・メソッド名・行数が出力されます。
  * [クラス#メソッド:行数]
  *
+ * ★ひとことメソッド紹介★
+ * いちいちLogTagを入れるの面倒ですよね。{@link Footprint}
+ * Log.dってキーボードで打ちにくいですよね。{@link #leave()}
+ * いちいち変数と文字列を"" + ""って書くの面倒ですよね。{@link #leave(Object...)}
+ * データクラスの全値を見やすく出したいですよね。{@link #json(Object)}
+ * トーストのLength指定が毎度めんどい、.show()を忘れるとかありますよね。{@link #toast(Context, String)}
+ * トーストすぐ消えちゃってデバッグにならないことありますよね。{@link #toast(Context, String, int)}
+ *
+ * メソッドの頭文字はなるべくホームポジションから動かなくていいものにしています
+ * (s,f,t,j,k,l)
+ *
  * Created by morayl on 2015/02/21.
  */
 public class Footprint {
     /** デフォルトのタグ */
     private static final String DEFAULT_TAG = "Footprint";
-    /** タグ */
-    private static String LOG_TAG = DEFAULT_TAG;
     /** デフォルトのインデント */
     private static final int DEFAULT_INDENT = 4;
+
+    /** タグ */
+    private static String LOG_TAG = DEFAULT_TAG;
+
+    /**
+     * タグを変更する
+     * デフォルトはFootprint
+     *
+     * @param logTag タグ名
+     */
+    public static void setLogTag(final String logTag) {
+        LOG_TAG = getStringValue(logTag);
+    }
+
+    /**
+     * クラス名、メソッド名、行数、メッセージをログに出力
+     * ※leaveメソッド郡は、StackTraceを参照するため、回数の多いfor文等で利用すると処理落ちする可能性があります
+     *
+     * @param message メッセージ
+     */
+    public static void leave(final Object message) {
+        simple(getMetaInfo(), getStringValue(message));
+    }
+
+    /**
+     * クラス名、メソッド名、行数をログに出力
+     *
+     * @see #leave(Object)
+     */
+    public static void leave() {
+        leave("");
+    }
+
+    /**
+     * クラス名、メソッド名、行数、メッセージをログに出力
+     * メッセージは順番に連結されます
+     * 文字列と変数をログ出力したい場合に、"" + valueと+を入力するのが面倒な人へ
+     *
+     * @param messages メッセージ
+     * @see #leave(Object)
+     */
+    public static void leave(final Object... messages) {
+        leave(unionObjectsString(messages));
+    }
+
+    /**
+     * Log.dを行う
+     *
+     * @param message メッセージ
+     */
+    public static void simple(final Object message) {
+        Log.d(LOG_TAG, getStringValue(message));
+    }
+
+    /**
+     * Log.dを行う
+     *
+     * @param messages メッセージ
+     */
+    public static void simple(final Object... messages) {
+        simple(unionObjectsString(messages));
+    }
+
+    /**
+     * クラス名、メソッド名、行数、メッセージをログに出力
+     * 引数は、キー,バリュー,キー,バリューという形で交互に入れることで、
+     * ・キー：バリュー
+     * ・キー：バリュー
+     * とログ出力されます
+     *
+     * @param keyValueKeyValue String,Object,String,Object...
+     */
+    public static void keyAndValues(final Object... keyValueKeyValue) {
+        if (isNullLeaveNull(keyValueKeyValue)) {
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < keyValueKeyValue.length; i++) {
+            final String message = getStringValue(keyValueKeyValue[i]);
+            if (i % 2 == 0) {
+                sb.append("\n ・");
+                sb.append(message);
+                sb.append(" : ");
+            } else {
+                sb.append(getStringValue(message));
+            }
+        }
+        leave(sb.toString());
+    }
+
+    /**
+     * 与えられた変数をjsonに整形して出力
+     * データ・モデルクラスでない場合で循環参照などがある場合には、
+     * {@link StackOverflowError}、{@link JSONException}が発生する可能性があります(内部でcatchされます)
+     *
+     * @param data データ
+     */
+    public static void json(Object data) {
+        json(data, DEFAULT_INDENT);
+    }
+
+    /**
+     * 与えられた変数のフィールドを全てログに出力する(インデント数指定)
+     *
+     * @param data データ
+     * @param indent インデントの幅
+     * @see #json(Object)
+     */
+    public static void json(Object data, final int indent) {
+        leave("\n", getFormattedJSON(data, indent));
+    }
+
+    /**
+     * 全てのフィールドとその値を出力する
+     *
+     * @param object 対象
+     */
+    public static void fields(Object object) {
+        if (isNullLeaveNull(object)) {
+            return;
+        }
+        try {
+            final JSONObject jsonObject = new JSONObject();
+            for (Field field : object.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                jsonObject.put(field.getName(), getStringValue(field.get(object)));
+            }
+            json(jsonObject);
+        } catch (Throwable t) {
+            stackTrace(t);
+        }
+    }
+
+    /**
+     * 渡されたThrowableのスタックトレースを出力
+     * {@link Throwable#printStackTrace()}をLogcatで再現する
+     *
+     * @param t 対象
+     */
+    public static void stackTrace(final Throwable t) {
+        leave(Log.getStackTraceString(t));
+    }
+
+    /**
+     * この時点でのスタックトレースを出力
+     * メソッドの呼び出し階層を確認したいときなどに
+     */
+    public static void stackTrace() {
+        stackTrace(new Throwable());
+    }
+
+    /**
+     * トーストを出力
+     *
+     * @param context {@link Context}
+     * @param message メッセージ
+     */
+    public static void toast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 指定秒数間トーストを表示し続ける
+     *
+     * @param context {@link Context}
+     * @param message メッセージ
+     * @param durationMillis 表示時間[ms](大体です)
+     */
+    public static void toast(final Context context, final String message, final int durationMillis) {
+        toast(context, message);
+        if (durationMillis > 1500) {
+            // 残り時間
+            final int leftDurationMillis = durationMillis - 1500;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    toast(context, message, leftDurationMillis);
+                }
+            }, leftDurationMillis > 1500 ? 1500 : leftDurationMillis);
+        }
+    }
 
     /**
      * Objectを文字列に変換
      *
-     * @param object
-     * @return
+     * @param object 変換対象
+     * @return 文字列
      */
     private static String getStringValue(Object object) {
         return String.valueOf(object);
@@ -42,6 +232,7 @@ public class Footprint {
 
     /**
      * ログ呼び出し元のメタ情報を取得する
+     * なければ"none"を返す
      *
      * @return [className#methodName:line]
      */
@@ -74,9 +265,9 @@ public class Footprint {
     /**
      * 整形されたJSON文字列を返す
      *
-     * @param object
-     * @param indent
-     * @return
+     * @param object 対象
+     * @param indent インデント
+     * @return json文字列
      */
     private static String getFormattedJSON(Object object, final int indent) {
         try {
@@ -95,10 +286,10 @@ public class Footprint {
     }
 
     /**
-     * 文字列を連結して返す
+     * 文字列をスペースで連結して返す
      *
-     * @param objects
-     * @return
+     * @param objects {@link Object}
+     * @return スペースで連結された文字列
      */
     private static String unionObjectsString(final Object... objects) {
         if (objects == null) {
@@ -109,14 +300,14 @@ public class Footprint {
             sb.append(getStringValue(object)).append(" ");
 
         }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
+        return sb.deleteCharAt(sb.length() - 1).toString();
     }
 
     /**
      * nullならnullログ出力してtrueを返す
      *
-     * @param object
+     * @param object 対象
+     * @return nullならtrue
      */
     private static boolean isNullLeaveNull(final Object object) {
         if (object == null) {
@@ -124,172 +315,5 @@ public class Footprint {
             return true;
         }
         return false;
-    }
-
-    /**
-     * クラス名、メソッド名、行数をログに出力
-     */
-    public static void leave() {
-        leave("");
-    }
-
-    /**
-     * クラス名、メソッド名、行数、メッセージをログに出力
-     */
-    public static void leave(final Object message) {
-        simple(getMetaInfo(), getStringValue(message));
-    }
-
-    /**
-     * クラス名、メソッド名、行数、メッセージをログに出力
-     * メッセージは順番に連結されます
-     * 文字列と変数をログ出力したい場合に、"" + valueと+を入力するのが面倒な人へ
-     *
-     * @param messages
-     */
-    public static void leave(final Object... messages) {
-        leave(unionObjectsString(messages));
-    }
-
-    /**
-     * Log.dを行う
-     *
-     * @param message
-     */
-    public static void simple(final Object message) {
-        Log.d(LOG_TAG, getStringValue(message));
-    }
-
-    /**
-     * Log.dを行う
-     *
-     * @param messages
-     */
-    public static void simple(final Object... messages) {
-        simple(unionObjectsString(messages));
-    }
-
-    /**
-     * クラス名、メソッド名、行数、メッセージをログに出力
-     * 引数は、キー,バリュー,キー,バリューという形で交互に入れることで、
-     * ■キー：バリュー
-     * ■キー：バリュー
-     * とログ出力されます
-     *
-     * @param keyValueKeyValue String,Object,String,Object...
-     */
-    public static void keyAndValues(final Object... keyValueKeyValue) {
-        if (isNullLeaveNull(keyValueKeyValue)) {
-            return;
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < keyValueKeyValue.length; i++) {
-            final String message = getStringValue(keyValueKeyValue[i]);
-            if (i % 2 == 0) {
-                sb.append("\n ・");
-                sb.append(message);
-                sb.append(" : ");
-            } else {
-                sb.append(getStringValue(message));
-            }
-        }
-        leave(sb.toString());
-    }
-
-    /**
-     * 与えられた変数のフィールドを全てログに出力する
-     *
-     * @param object
-     */
-    public static void json(Object object) {
-        json(object, DEFAULT_INDENT);
-    }
-
-    /**
-     * 与えられた変数のフィールドを全てログに出力する
-     *
-     * @param object
-     * @param indent インデントの幅
-     * @return
-     */
-    public static void json(Object object, final int indent) {
-        leave("\n", getFormattedJSON(object, indent));
-    }
-
-    /**
-     * 全てのフィールドとその値を出力する
-     *
-     * @param object
-     */
-    public static void fields(Object object) {
-        if (isNullLeaveNull(object)) {
-            return;
-        }
-        try {
-            final JSONObject jsonObject = new JSONObject();
-            for (Field field : object.getClass().getDeclaredFields()) {
-                field.setAccessible(true);
-                jsonObject.put(field.getName(), getStringValue(field.get(object)));
-            }
-            json(jsonObject);
-        } catch (Throwable t) {
-            stackTrace(t);
-        }
-    }
-
-    /**
-     * 渡されたThrowableのスタックトレースを出力
-     *
-     * @param t
-     */
-    public static void stackTrace(final Throwable t) {
-        leave(Log.getStackTraceString(t));
-    }
-
-    /**
-     * この時点でのスタックトレースを出力
-     */
-    public static void stackTrace() {
-        stackTrace(new Throwable());
-    }
-
-    /**
-     * トーストを出力する
-     *
-     * @param context
-     * @param message
-     */
-    public static void toast(Context context, String message) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * 指定秒数間トーストを表示し続ける
-     *
-     * @param context
-     * @param message
-     * @param durationMillis
-     */
-    public static void toast(final Context context, final String message, final int durationMillis) {
-        toast(context, message);
-        if (durationMillis > 1500) {
-            // 残り時間
-            final int leftDurationMillis = durationMillis - 1500;
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    toast(context, message, leftDurationMillis);
-                }
-            }, leftDurationMillis > 1500 ? 1500 : leftDurationMillis);
-        }
-    }
-
-    /**
-     * タグを変更する
-     *
-     * @param logTag
-     */
-    public static void changeLogTag(final String logTag) {
-        LOG_TAG = getStringValue(logTag);
     }
 }
