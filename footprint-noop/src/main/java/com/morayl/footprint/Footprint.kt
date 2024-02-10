6c8838fa5a89c768ced611d6ef7eea7e1b90fc10
morayl <sea.gang.moray@gmail.com>
2024-02-10 18:47:09 +0900

    Revert "Make all methods deprecated"
    This reverts commit 94537d1884c1b6588b2528c86188905cc3c70551.

--- footprint-noop/src/main/java/com/morayl/footprint/Footprint.kt
+++ footprint-noop/src/main/java/com/morayl/footprint/Footprint.kt
@@ -2,9 +2,6 @@ package com.morayl.footprint
 
 /**
  * NoOp
- * footprint-ktx has been deprecated.
- * Please see below for the migration.
- * https://github.com/morayl/Footprint/blob/master/CHANGELOG.md
  */
 
 private var enableInternal = true
@@ -18,7 +15,6 @@ private var defaultJsonIndentCount = 4
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun configFootprint(
         enable: Boolean = enableInternal,
         logTag: String = defaultLogTag,
@@ -33,49 +29,42 @@ fun configFootprint(
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun footprint(priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun footprint(vararg messages: Any?, priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun accentFootprint(vararg messages: Any? = emptyArray(), logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun simpleFootprint(message: Any?, priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun simpleFootprint(vararg messages: Any?, priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun jsonFootprint(target: Any?, indent: Int = defaultJsonIndentCount, priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun <T> T.withJsonFootprint(indent: Int = defaultJsonIndentCount, priority: LogPriority = defaultLogPriority,
                             logTag: String = defaultLogTag, block: (T) -> Any? = { this }): T {
     return this
@@ -84,7 +73,6 @@ fun <T> T.withJsonFootprint(indent: Int = defaultJsonIndentCount, priority: LogP
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun <T> T.withFootprint(priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag, block: (T) -> Any? = { this }): T {
     return this
 }
@@ -92,7 +80,6 @@ fun <T> T.withFootprint(priority: LogPriority = defaultLogPriority, logTag: Stri
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun <T> T.withSimpleFootprint(priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag, block: (T) -> Any? = { this }): T {
     return this
 }
@@ -100,20 +87,17 @@ fun <T> T.withSimpleFootprint(priority: LogPriority = defaultLogPriority, logTag
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun pairFootprint(vararg pairs: Pair<String, Any?>, priority: LogPriority = defaultLogPriority, logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun Throwable.stacktraceFootprint(priority: LogPriority = defaultStackTraceLogLevel, logTag: String = defaultLogTag) {
 }
 
 /**
  * NoOp
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 fun stacktraceFootprint(priority: LogPriority = defaultStackTraceLogLevel, logTag: String = defaultLogTag) {
 }
--- footprint-noop/src/main/java/com/morayl/footprint/LogPriority.kt
+++ footprint-noop/src/main/java/com/morayl/footprint/LogPriority.kt
@@ -2,11 +2,7 @@ package com.morayl.footprint
 
 /**
  * NoOp.
- * footprint-ktx has been deprecated.
- * Please see below for the migration.
- * https://github.com/morayl/Footprint/blob/master/CHANGELOG.md
  */
-@Deprecated(message = "footprint-ktx has been deprecated. Please see github for the migration. https://github.com/morayl/Footprint/blob/master/CHANGELOG.md ")
 enum class LogPriority(val value: Int) {
     VERBOSE(2),
     DEBUG(3),
